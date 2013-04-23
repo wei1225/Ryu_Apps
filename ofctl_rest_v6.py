12410be3963302bc1e1824f7802a62e024a59dd3
sjoe <chaojiong.zheng@gmail.com>
2013-04-23 11:27:28 +0800

    first commit

---
+++ ofctl_rest_v6.py
@@ -0,0 +1,282 @@
+# Copyright (C) 2012 Nippon Telegraph and Telephone Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+import json
+import urllib
+import ast
+from webob import Response
+#import sys   
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller import dpset
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_0
+import ofctl_v6
+#from ryu.lib import ofctl_v1_0
+from ryu.app.wsgi import ControllerBase, WSGIApplication
+
+
+LOG = logging.getLogger('ryu.app.ofctl_rest_v6')
+
+# REST API
+#
+## Retrieve the switch stats
+#
+# get the list of all switches
+# GET /stats/switches
+#
+# get the desc stats of the switch
+# GET /stats/desc/<dpid>
+#
+# get flows stats of the switch
+# GET /stats/flow/<dpid>
+#
+# get ports stats of the switch
+# GET /stats/port/<dpid>
+#
+## Update the switch stats
+#
+# add a flow entry
+# POST /stats/flowentry/add
+#
+# modify all matching flow entries
+# POST /stats/flowentry/modify
+#
+# delete all matching flow entries
+# POST /stats/flowentry/delete
+#
+# delete all flow entries of the switch
+# DELETE /stats/flowentry/clear/<dpid>
+#
+
+
+class StatsController(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(StatsController, self).__init__(req, link, data, **config)
+        self.dpset = data['dpset']
+        self.waiters = data['waiters']
+
+    def get_homepage(self, req, **_kwargs):
+        #dps = self.dpset.dps.keys()
+        #body = json.dumps(dps)
+        return (Response(content_type='text/html', 
+            body='<a href="/stats/switches">get the list of all switches</a><br/>'))
+        #return Response(status=404)
+
+
+    def get_dpids(self, req, **_kwargs):
+
+        dps = self.dpset.dps.keys()
+        body = json.dumps(dps)
+        return (Response(content_type='application/json', body=body))
+
+    def get_desc_stats(self, req, dpid, **_kwargs):
+        dp = self.dpset.get(int(dpid))
+        if dp is None:
+            return Response(status=404)
+
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            desc = ofctl_v6.get_desc_stats(dp, self.waiters)
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+
+        body = json.dumps(desc)
+        return (Response(content_type='application/json', body=body))
+
+    def get_flow_stats(self, req, dpid, **_kwargs):
+        dp = self.dpset.get(int(dpid))
+        if dp is None:
+            return Response(status=404)
+
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            flows = ofctl_v6.get_flow_stats(dp, self.waiters)
+            #print flows
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+               
+        body = json.dumps(flows,indent=4, separators=(',', ': '),encoding='ISO-8859-1')        
+        return (Response(content_type='application/json',body = body))
+       
+
+    def get_port_stats(self, req, dpid, **_kwargs):
+        dp = self.dpset.get(int(dpid))
+        if dp is None:
+            return Response(status=404)
+
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            ports = ofctl_v6.get_port_stats(dp, self.waiters)
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+
+        body = json.dumps(ports)
+        return (Response(content_type='application/json', body=body))
+
+    def mod_flow_entry(self, req, cmd, **_kwargs):
+
+        #delete {'dpid':1,'actions':[{'type':'output','port':2}} actions is a list of dict
+        #add {'dpid':1,'actions':[{'type':'OUTPUT','port':1}],'match':{'in_port':2}}
+        #modify is add a new entry not really modify
+        #
+        #print req.body
+        #{'dpid':1,'rule':{'dl_type':0x86dd,'ipv6_src':'10:0:0:0:0:0:0:1'},'actions':[{'type':OUTPUT,'port':2}]}
+        input = urllib.unquote(req.body)
+        #text1={'dpid':'1'}&Submit=Delete  delete
+        #text1={'dpid':'1'}
+        #Submit=Delete                      modify '\n' ?
+        
+
+        input_ = urllib.unquote(req.body)
+        print input_
+        
+        ftemp = input_.split('=')[1]
+
+        try:
+            flow = eval(ftemp)
+            #print flow                      
+        except SyntaxError:
+
+            LOG.debug('invalid syntax %s', req.body)
+            return Response(status=400)
+
+        dpid = flow.get('dpid')
+        dp = self.dpset.get(int(dpid))
+        if dp is None:
+            return 'error'
+        #print flow,cmd
+        if cmd == 'add':
+            cmd = dp.ofproto.OFPFC_ADD
+        elif cmd == 'modify':
+            cmd = dp.ofproto.OFPFC_MODIFY
+        elif cmd == 'delete':
+            cmd = dp.ofproto.OFPFC_DELETE     
+        else:
+            return 'method error'
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            ofctl_v6.mod_flow_entry(dp, flow, cmd)
+                          
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return 'unsupport of protocol'
+
+        return 'success'
+
+    def delete_flow_entry(self, req, dpid, **_kwargs):
+        dp = self.dpset.get(int(dpid))
+        if dp is None:
+            return Response(status=404)
+
+        if dp.ofproto.OFP_VERSION == ofproto_v1_0.OFP_VERSION:
+            ofctl_v6.delete_flow_entry(dp)
+        else:
+            LOG.debug('Unsupported OF protocol')
+            return Response(status=501)
+
+        return Response(status=200)
+
+           
+class RestStatsApi(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
+    _CONTEXTS = {
+        'dpset': dpset.DPSet,
+        'wsgi': WSGIApplication
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(RestStatsApi, self).__init__(*args, **kwargs)
+        self.dpset = kwargs['dpset']
+        wsgi = kwargs['wsgi']
+        self.waiters = {}
+        self.data = {}
+        self.data['dpset'] = self.dpset
+        self.data['waiters'] = self.waiters
+        mapper = wsgi.mapper
+
+        wsgi.registory['StatsController'] = self.data
+        
+        path = '/stats'
+
+        mapper.connect('/',
+                       controller=StatsController, action='get_homepage',
+                       conditions=dict(method=['GET']))
+        uri = path + '/switches'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='get_dpids',
+                       conditions=dict(method=['GET']))
+
+        uri = path + '/desc/{dpid}'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='get_desc_stats',
+                       conditions=dict(method=['GET']))
+
+        uri = path + '/flow/{dpid}'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='get_flow_stats',
+                       conditions=dict(method=['GET']))
+
+        uri = path + '/port/{dpid}'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='get_port_stats',
+                       conditions=dict(method=['GET']))
+
+        uri = path + '/flowentry/{cmd}'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='mod_flow_entry',
+                       conditions=dict(method=['POST']))
+
+        uri = path + '/flowentry/clear/{dpid}'
+        mapper.connect('stats', uri,
+                       controller=StatsController, action='delete_flow_entry',
+                       conditions=dict(method=['POST']))
+
+    def stats_reply_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+
+        if dp.id not in self.waiters:
+            return
+        if msg.xid not in self.waiters[dp.id]:
+            return
+        lock, msgs = self.waiters[dp.id][msg.xid]
+        msgs.append(msg)
+
+        if msg.flags & dp.ofproto.OFPSF_REPLY_MORE:
+            return
+        del self.waiters[dp.id][msg.xid]
+        lock.set()
+
+    @set_ev_cls(ofp_event.EventOFPDescStatsReply, MAIN_DISPATCHER)
+    def desc_stats_reply_handler(self, ev):
+        self.stats_reply_handler(ev)
+    '''
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def flow_stats_reply_handler(self, ev):
+        self.stats_reply_handler(ev)
+        print 'hello 1234'
+    '''
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def port_stats_reply_handler(self, ev):
+        self.stats_reply_handler(ev)
+    
+    @set_ev_cls(ofp_event.EventNXFlowStatsReply, MAIN_DISPATCHER)   
+    def flow_stats_reply_handler(self, ev):
+        self.stats_reply_handler(ev)        
